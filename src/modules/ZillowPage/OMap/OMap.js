014ed7565a8d1ae2114c44636c6df64acd920c65
Danny <danny@atlasstud.io>
2021-03-15 17:04:27 -0700

    final fix

--- src/modules/ZillowPage/OMap/OMap.js
+++ src/modules/ZillowPage/OMap/OMap.js
@@ -13,6 +13,11 @@ import './OMap.css';
 
 import stateGeoJSON from '../../../data/usa_states_zhvi.geojson';
 
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 function OMap({ name, setName, price, setPrice, year }) {
 
     const [map, setMap] = useState();
