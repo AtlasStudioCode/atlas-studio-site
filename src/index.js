1110c2f0eab37f51bdf53e99168ab5be720b7bbd
Danny <danny@atlasstud.io>
2021-03-30 10:15:21 -0700

    aws-exports

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './modules/App/App';
 
 import Amplify from 'aws-amplify';
-import awsconfig from 'aws-exports';
-Amplify.configure(awsconfig);
+import config from './aws-exports';
+Amplify.configure(config);
 
 // render the main app at the root div element
 ReactDOM.render(
