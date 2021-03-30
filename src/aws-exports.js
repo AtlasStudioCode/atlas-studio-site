a374cb2939523f7b6a68a9ab864d0da89016ec7d
Danny <danny@atlasstud.io>
2021-03-30 10:24:58 -0700

    add storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,9 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_AouIZfC9T",
     "aws_user_pools_web_client_id": "7lh4a6u21eufaq307a4i9fqnuh",
-    "oauth": {}
+    "oauth": {},
+    "aws_user_files_s3_bucket": "s3atlasstudiobucket172449-staging",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
